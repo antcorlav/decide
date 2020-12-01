3c7368f73be159c55b13174647d8d44dc4d323a0
antcorlav <antcorlav@alum.us.es>
2020-12-01 11:40:36 +0100

    (tipo) Encabezado practica 19.30
    Cuerpo del commit
    contenido

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'complexpassword',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
